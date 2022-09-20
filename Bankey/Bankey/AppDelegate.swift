219f8c4b74ef6e78d664ca2b21574c25b21316e3
JayoonKoo <rnwkdbs12@gmail.com>
2022-09-21 00:55:20 +0900

    conf: project setting
    - no storyboard

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by 구자윤 on 2022/09/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
