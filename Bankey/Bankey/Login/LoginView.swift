65f590a4ca2b738cc4834de1495951211d7255d2
jayoonkoo <rnwkdbs12@gmail.com>
2022-09-22 00:16:28 +0900

    feat: LoginController 생성, LoginView 추가

---
+++ Bankey/Bankey/Login/LoginView.swift
@@ -0,0 +1,35 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by 구자윤 on 2022/09/21.
+//
+import UIKit
+
+class LoginView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .orange
+    }
+    
+    func layout() {
+        
+    }
+}
