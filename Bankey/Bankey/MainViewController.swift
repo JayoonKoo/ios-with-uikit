0d49390d10cd66b94681d411627bcab1d5d53edc
jayoonkoo <rnwkdbs12@gmail.com>
2022-09-28 23:29:03 +0900

    feat: MainViewController 추가하기

--- Bankey/Bankey/MainViewController.swift
+++ Bankey/Bankey/MainViewController.swift
@@ -12,18 +12,25 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+class DummyVC1: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
     }
-    */
+}
 
+class DummyVC2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+class DummyVC3: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
 }
