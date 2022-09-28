09c3bd81a05021a7ec724803a27e68d34acd0965
jayoonkoo <rnwkdbs12@gmail.com>
2022-09-28 23:29:22 +0900

    feat: LocalState 추가하기

--- Bankey/Bankey/Utils/LocalState.swift
+++ Bankey/Bankey/Utils/LocalState.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class LocalState {
+    private enum Keys: String {
+        case hasOnboarded
+    }
+    
+    static var hasOnboarded: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: Keys.hasOnboarded.rawValue)
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
+        }
+    }
+}
