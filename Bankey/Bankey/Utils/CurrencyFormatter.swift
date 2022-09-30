75893f41f7bdb510484eba94ec656afbc2060450
jayoonkoo <rnwkdbs12@gmail.com>
2022-09-30 22:35:58 +0900

    feat: Utils 추가
    - CurrencyFormatter -> 통화 포멧터
    - UIViewController+Utils -> statusBar tabBar 셋팅

--- Bankey/Bankey/Utils/CurrencyFormatter.swift
+++ Bankey/Bankey/Utils/CurrencyFormatter.swift
@@ -4,5 +4,70 @@
 //
 //  Created by 구자윤 on 2022/09/30.
 //
-
 import Foundation
+import UIKit
+
+class CurrencyFormatter {
+    func makeBalanceAmountText(balance: Decimal) -> NSAttributedString {
+        let dallarCents = breakDollarsIntoCents(balnce: balance)
+        let dallars = dallarCents.0
+        let cents = dallarCents.1
+        return makeBalanceAmountText(dollars: dallars, cents: cents)
+    }
+    
+    func breakDollarsIntoCents(balnce: Decimal) -> (String, String) {
+        let balnceDouble = NSDecimalNumber(decimal: balnce).doubleValue
+        
+        let modfMoney = modf(balnceDouble)
+        
+        let dollars = convertDollar(modfMoney.0)
+        let cents = convertCents(modfMoney.1)
+        
+        return (dollars, cents)
+    }
+    
+    func convertDollar(_ dallar: Double) -> String {
+        let formattedDallar = formatDallar(dallar)
+        var result = formattedDallar.split(separator: ".")[0]
+        result.removeFirst()
+        return String(result)
+    }
+    
+    func convertCents(_ cents: Double) -> String {
+        let result: String
+        if cents == 0 {
+            result = "00"
+        } else {
+            result = String(format: "%.0f", cents * 100)
+        }
+        return result
+    }
+    
+    func formatDallar(_ dallar: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        
+        if let result = formatter.string(from: dallar as NSNumber) {
+            return result
+        }
+        
+        return ""
+    }
+    
+    private func makeBalanceAmountText(dollars: String, cents: String) -> NSMutableAttributedString {
+        let dollarsSignAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
+        let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
+        let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .footnote), .baselineOffset: 8]
+        
+        let rootString = NSMutableAttributedString(string: "$", attributes: dollarsSignAttributes)
+        let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
+        let centString = NSAttributedString(string: cents, attributes: centAttributes)
+        
+        rootString.append(dollarString)
+        rootString.append(centString)
+        
+        return rootString
+    }
+}
--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 extension UIViewController {
+    func setStatuBar() {
+        let navAppearnce = UINavigationBarAppearance()
+        navAppearnce.configureWithTransparentBackground()
+        navAppearnce.backgroundColor = appColor
+        
+        UINavigationBar.appearance().standardAppearance = navAppearnce
+        UINavigationBar.appearance().scrollEdgeAppearance = navAppearnce
+    }
     func setTabBarImage(imageName: String, title: String) {
         let configuration = UIImage.SymbolConfiguration(scale: .large)
         let image = UIImage(systemName: imageName, withConfiguration: configuration)
